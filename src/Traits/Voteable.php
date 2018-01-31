ba1259103991e729dfb7ff9ff9027f33b8c5ae74
Younes Khoubza <nesyou2013@gmail.com>
2018-01-31 23:41:09 +0000

    update and delete votes

--- src/Traits/Voteable.php
+++ src/Traits/Voteable.php
@@ -20,36 +20,65 @@ trait Voteable
         return $this->morphMany(Vote::class, 'voteable');
     }
 
+    /**
+     * @return mixed
+     */
     public function upVotesCount()
     {
         return $this->votes()->where('score', '>=', 0)->sum('score');
     }
 
+    /**
+     * @return mixed
+     */
     public function downVotesCount()
     {
         return $this->votes()->where('score', '<', 0)->sum('score');
     }
 
+    /**
+     * @return bool
+     */
     public function isUpVotes()
     {
         return $this->votes()->where('score', '>=', 0)->exists();
     }
 
+    /**
+     * @return bool
+     */
     public function isDownVoted()
     {
         return $this->votes()->where('score', '<', 0)->exists();
     }
 
+    /**
+     * @param int $user_id
+     *
+     * @return bool
+     */
     public function isUpVotedByUser(int $user_id)
     {
         return $this->votes()->where('score', '>=', 0)->where('user_id', $user_id)->exists();
     }
 
+    /**
+     * @param int $user_id
+     *
+     * @return bool
+     */
     public function isDownVotedByUser(int $user_id)
     {
         return $this->votes()->where('score', '<', 0)->where('user_id', $user_id)->exists();
     }
 
+    /**
+     * @param Builder $query
+     * @param string $direction
+     * @param string $type
+     *
+     * @return Builder
+     */
     public function scopeOrderByUpVotes(Builder $query, string $direction = 'asc', string $type = '>=')
     {
         return $query
@@ -64,8 +93,64 @@ trait Voteable
             ->orderBy('count_votes', $direction);
     }
 
+    /**
+     * @param Builder $query
+     * @param string $direction
+     *
+     * @return Builder
+     */
     public function scopeOrderByDownVotes(Builder $query, string $direction = 'asc')
     {
         return $this->scopeOrderByUpVotes($query, $direction, '<');
+    }
+
+    /**
+     * @param int $vote_id
+     *
+     * @return mixed
+     */
+    public function deleteVote(int $vote_id)
+    {
+        return $this->votes()->where('id', $vote_id)->delete();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function resetVotes()
+    {
+        return $this->votes()->delete();
+    }
+
+    /**
+     * @param int $user_id
+     *
+     * @return mixed
+     */
+    public function deleteVotesForUser(int $user_id)
+    {
+        return $this->votes()->where('user_id', $user_id)->delete();
+    }
+
+    /**
+     * @param int $user_id
+     * @param int $score
+     *
+     * @return int
+     */
+    public function updateVotesForUser(int $user_id, int $score)
+    {
+        return $this->votes()->where('user_id', $user_id)->update(['score' => $score]);
+    }
+
+    /**
+     * @param int $vote_id
+     * @param int $score
+     *
+     * @return int
+     */
+    public function updateRating(int $vote_id, int $score)
+    {
+        return $this->votes()->where('id', $vote_id)->update(['score' => $score]);
     }
 }
