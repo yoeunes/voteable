0213f9196350e75741380a5cfcf497a6175fe80b
Younes Khoubza <nesyou2013@gmail.com>
2018-01-31 23:21:51 +0000

    add Vote model

---
+++ src/Models/Vote.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Yoeunes\Voteable\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vote extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'score',
+        'user_id',
+        'voteable_id',
+        'voteable_type',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function voteable()
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(config('voteable.user'));
+    }
+}
