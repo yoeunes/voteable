ed46a42e16b9b481c8dd56c82a1ac69e0d5c2032
Younes Khoubza <nesyou2013@gmail.com>
2018-01-31 23:18:40 +0000

    create migrations file

---
+++ migrations/create_votes_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVotesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('score');
+            $table->morphs('voteable');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
