c266d5c685529fd0a5b3491641e4576ddaf654af
Younes Khoubza <nesyou2013@gmail.com>
2018-02-02 20:07:12 +0000

    test up vote count

--- tests/RateableTest.php
+++ tests/RateableTest.php
@@ -8,6 +8,7 @@ use Yoeunes\Voteable\Tests\Stubs\Models\Lesson;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Yoeunes\Voteable\Tests\Stubs\Models\User;
 
 class RateableTest extends TestCase
 {
@@ -33,5 +34,22 @@ class RateableTest extends TestCase
         /** @var Lesson $lesson */
         $lesson = Factory::create(Lesson::class);
         $this->assertInstanceOf(MorphMany::class, $lesson->votes());
+    }
+
+    /** @test */
+    public function it_get_up_vote_count()
+    {
+        /** @var Lesson $lesson */
+        $lesson = Factory::create(Lesson::class);
+
+        /** @var User $user */
+        $user = Factory::create(User::class);
+
+        Factory::create(Vote::class, ['voteable_id' => $lesson->id, 'amount' => +1, 'user_id' => $user->id]);
+        Factory::create(Vote::class, ['voteable_id' => $lesson->id, 'amount' => -2, 'user_id' => $user->id]);
+        Factory::create(Vote::class, ['voteable_id' => $lesson->id, 'amount' => +3]);
+
+        $this->assertEquals(4, $lesson->upVotesCount());
+        $this->assertEquals(-2, $lesson->downVotesCount());
     }
 }
